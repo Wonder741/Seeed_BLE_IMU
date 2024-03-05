7b0fd16138bc19e00f2be1de3d4ef46c133f2907
Wonder741 <cgwang430@gmail.com>
2024-03-05 10:04:04 +1000

    Arduino code
    One for raw data and one for float data

---
+++ BLE_RTOS_IMU_BAT_3/build/libraries/Adafruit_TinyUSB_Arduino/class/usbtmc/usbtmc_device.c.d
@@ -0,0 +1,6 @@
+c:\Users\uqcwan34\OneDrive\ -\ The\ University\ of\ Queensland\Documents\GitHub\Smart_orthotics_hardware\BLE_RTOS_IMU_BAT_1\build\libraries\Adafruit_TinyUSB_Arduino\class\usbtmc\usbtmc_device.c.o: \
+ C:\Users\uqcwan34\AppData\Local\Arduino15\packages\Seeeduino\hardware\nrf52\1.1.8\libraries\Adafruit_TinyUSB_Arduino\src\class\usbtmc\usbtmc_device.c \
+ C:\Users\uqcwan34\AppData\Local\Arduino15\packages\Seeeduino\hardware\nrf52\1.1.8\libraries\Adafruit_TinyUSB_Arduino\src/tusb_option.h \
+ C:\Users\uqcwan34\AppData\Local\Arduino15\packages\Seeeduino\hardware\nrf52\1.1.8\libraries\Adafruit_TinyUSB_Arduino\src/common/tusb_compiler.h \
+ C:\Users\uqcwan34\AppData\Local\Arduino15\packages\Seeeduino\hardware\nrf52\1.1.8\libraries\Adafruit_TinyUSB_Arduino\src/tusb_config.h \
+ C:\Users\uqcwan34\AppData\Local\Arduino15\packages\Seeeduino\hardware\nrf52\1.1.8\libraries\Adafruit_TinyUSB_Arduino\src/arduino/ports/nrf/tusb_config_nrf.h
